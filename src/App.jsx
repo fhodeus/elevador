01afe41e38cb9d1d26fcc1c83492e2d9ae80317e
Fernando Henrique <bxnandoxd@hotmail.com>
2022-07-28 18:58:46 -0300

    teste clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Elevator } from "./components/Elevator";
 import { BoardControl } from "./components/BoardControl";
 
+import { Helmet } from "react-helmet";
+
 import { ElevatorProvider } from "./hooks/ElevatorContext";
 
 export const App = () => (
-  <div className="container">
-    <ElevatorProvider>
-      <div className="row">
-        <div className="col-4 col-md-3">
-          <Elevator />
+  <>
+    <Helmet>
+      <script type="text/javascript" src="/clarity.js"></script>
+    </Helmet>
+    <div className="container">
+      <ElevatorProvider>
+        <div className="row">
+          <div className="col-4 col-md-3">
+            <Elevator />
+          </div>
+          <div className="mx-auto mx-sm-0 col-7 col-lg-3 ">
+            <BoardControl />
+          </div>
         </div>
-        <div className="mx-auto mx-sm-0 col-7 col-lg-3 ">
-          <BoardControl />
-        </div>
-      </div>
-    </ElevatorProvider>
-  </div>
+      </ElevatorProvider>
+    </div>
+  </>
 );
 
 export default App;
